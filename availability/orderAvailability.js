5c9cae109b2ac0f7de68bb1cb046e461544c935e
vaibhav <vaibhavbhardwaaj@gmail.com>
2020-07-20 18:16:32 +0530

    added dynamic upi

--- availability/orderAvailability.js
+++ availability/orderAvailability.js
@@ -4,10 +4,14 @@ const router = express.Router();
 let isAvailable = true;
 let deliveryPrice = 22;
 
+let upiId = "7081256474@ybl"
+let upiName = "Prakhar Bhatt"
 
 router.get('/',(req,res)=>{
 	res.json({
-		deliveryPrice:deliveryPrice
+		deliveryPrice:deliveryPrice,
+		upiId:upiId,
+		upiName:upiName
 	})
 })
 
